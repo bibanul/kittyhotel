b7db443332200469f5ca46db04f450555a72515a
Bogdan Cirlig <bc01@cirlig.com>
2013-01-22 18:32:02 -0500

    errors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express();
+var app = module.exports = express.createServer();
 
 // Configuration
 
@@ -32,4 +32,4 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 app.listen(3000);
-//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
